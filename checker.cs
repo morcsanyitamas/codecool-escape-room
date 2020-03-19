67134cb4f13deb264da608d7962a530febe1fd5e
morcsanyitamas <mtomi8@gmail.com>
2020-03-19 14:19:48 +0100

    initial commit

---
+++ checker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace escapeRoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String secretCode = requestSecretCode(null);
+            while (secretCode != 2015.ToString())
+            {
+                Console.Clear();
+                secretCode = requestSecretCode("Your secret code is not valid! Please try again! ");
+            }
+            Console.Clear();
+            Console.WriteLine("Congratulation! Your secret code is valid!");
+        }
+
+        static String requestSecretCode(String message)
+        {
+            if (message != null)
+            {
+                Console.WriteLine(message);
+            }
+            Console.Write("Please provide the secret code: ");
+            return Console.ReadLine();
+        }
+    }
+}
